e1841d5d14730af85a02ba7abc7874a00a9c488e
Leo <leononame@lho.io>
2019-12-23 17:18:01 +0100

    add config

---
+++ config.py
@@ -0,0 +1,30 @@
+import os
+
+from dataclasses import dataclass
+from string import Template
+
+
+@dataclass
+class Config:
+    bean_append_file: str
+    bean_file: str
+
+    @classmethod
+    def load(cls):
+        c = cls(
+            bean_append_file=os.environ.get("BEAN_APPEND_FILE"),
+            bean_file=os.environ.get("BEAN_FILE")
+        )
+        return c
+
+    def check(self) -> str:
+        tpl = "Configuration error: {} is invalid. (value:{})"
+
+        if self is None:
+            return None
+        if not self.bean_append_file:
+            return tpl.format("BEAN_APPEND_FILE", self.bean_append_file)
+        if not self.bean_file:
+            return tpl.format("BEAN_FILE", self.bean_file)
+
+        return None
--- main.py
+++ main.py
@@ -1,15 +1,34 @@
 import logging
 import sys
 
+from config import Config
 from beancount import loader
 from beancount.core.data import Open as Account
 
+
 def get_accounts(fname: str) -> list:
-	entries, errors, options_map = loader.load_file(fname,
-            log_timings=logging.info,
-            log_errors=sys.stderr)
-	if errors:
-		return None
-	expenses = options_map['name_expenses']
-	accounts = [e.account.split(expenses+":")[1] for e in entries if type(e) is Account and e.account.startswith(expenses)]
-	return accounts
+    entries, errors, options_map = loader.load_file(fname,
+                                                    log_timings=logging.info,
+                                                    log_errors=sys.stderr)
+    if errors:
+        return None
+    # Expense prefix is usually Expenses:, but might be something else throught the options
+    prefix = options_map['name_expenses'] + ":"
+    # Filter for expense accounts and strip the prefix
+    accounts = [e.account.split(prefix)[1] for e in entries
+                if type(e) is Account and e.account.startswith(prefix)]
+    return sorted(accounts)
+
+
+def main():
+    c = Config.load()
+    err = c.check()
+    if err:
+        print(err)
+        exit(1)
+    print(get_accounts(c.bean_file))
+    exit(0)
+
+
+if __name__ == "__main__":
+    main()
